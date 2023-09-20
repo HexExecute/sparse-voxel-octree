c7af3193d17b4af0c369b00347daefa9506bb9e9
HexExecute <hex.execute@gmail.com>
2023-09-19 21:19:32 -0400

    Refactored

--- src/lib.rs
+++ src/lib.rs
@@ -2,34 +2,13 @@
 mod tests {
     use crate::*;
 
-    // #[test]
-    // fn get() {
-    //     let tree = SparseVoxelOctree::new(2);
-    //
-    //     assert_eq!(tree.get(0, 0, 0), Some(&Voxel { index: 0 }));
-    //     assert_eq!(tree.get(1, 0, 0), Some(&Voxel { index: 1 }));
-    //     assert_eq!(tree.get(0, 1, 0), Some(&Voxel { index: 2 }));
-    //     assert_eq!(tree.get(1, 1, 0), Some(&Voxel { index: 3 }));
-    //     assert_eq!(tree.get(0, 0, 1), Some(&Voxel { index: 4 }));
-    //     assert_eq!(tree.get(1, 0, 1), Some(&Voxel { index: 5 }));
-    //     assert_eq!(tree.get(0, 1, 1), Some(&Voxel { index: 6 }));
-    //     assert_eq!(tree.get(1, 1, 1), Some(&Voxel { index: 7 }));
-    // }
-
     #[test]
     fn insert() {
         let mut tree = SparseVoxelOctree::new(1);
 
-        // dbg!(&tree);
-
-        assert_eq!(tree.get(1, 1, 1), Some(&Voxel { index: 7 }));
-
         tree.insert(0, 0, 0, Node::Leaf(Some(Voxel { index: 9999 })), 3);
 
-        dbg!(tree.root);
-        // dbg!(tree.get(1, 0, 0));
-
-        // assert_eq!(tree.get(0, 0, 0));
+        dbg!(tree.get(0, 0, 0));
     }
 }
 
@@ -75,16 +54,7 @@ impl Node {
             Node::Leaf(Some(Voxel::new()))
         } else {
             Node::Branch {
-                children: Box::new([
-                    Node::new(depth - 1),
-                    Node::new(depth - 1),
-                    Node::new(depth - 1),
-                    Node::new(depth - 1),
-                    Node::new(depth - 1),
-                    Node::new(depth - 1),
-                    Node::new(depth - 1),
-                    Node::new(depth - 1),
-                ]),
+                children: Box::new(std::array::from_fn(|_| Node::new(depth - 1))),
             }
         }
     }
@@ -93,18 +63,10 @@ impl Node {
         match self {
             Node::Leaf(voxel) => voxel.as_ref(),
             Node::Branch { children } => {
-                let size = size / 2;
+                let s = size / 2;
+                let index = ((x >= s) as usize) << 0 | ((y >= s) as usize) << 1 | ((z >= s) as usize) << 2;
 
-                match (x < size, y < size, z < size) {
-                    (true, true, true) => children[0].get(x, y, z, size), // 000
-                    (false, true, true) => children[1].get(x - size, y, z, size), // 100
-                    (true, false, true) => children[2].get(x, y - size, z, size), // 010
-                    (false, false, true) => children[3].get(x - size, y - size, z, size), // 110
-                    (true, true, false) => children[4].get(x, y, z - size, size), // 001
-                    (false, true, false) => children[5].get(x - size, y, z - size, size), // 101
-                    (true, false, false) => children[6].get(x, y - size, z - size, size), // 011
-                    (false, false, false) => children[7].get(x - size, y - size, z - size, size), // 111
-                }
+                children[index].get(x % s, y % s, z % s, s)
             }
         }
     }
@@ -117,46 +79,15 @@ impl Node {
 
         match self {
             Node::Branch { children } => {
-                let size = size / 2;
+                let s = size / 2;
+                let index = ((x >= s) as usize) << 0 | ((y >= s) as usize) << 1 | ((z >= s) as usize) << 2;
 
-                match (x < size, y < size, z < size) {
-                    (true, true, true) => children[0].insert(x, y, z, node, size, depth - 1), // 000
-                    (false, true, true) => {
-                        children[1].insert(x - size, y, z, node, size, depth - 1)
-                    } // 100
-                    (true, false, true) => {
-                        children[2].insert(x, y - size, z, node, size, depth - 1)
-                    } // 010
-                    (false, false, true) => {
-                        children[3].insert(x - size, y - size, z, node, size, depth - 1)
-                    } // 110
-                    (true, true, false) => {
-                        children[4].insert(x, y, z - size, node, size, depth - 1)
-                    } // 001
-                    (false, true, false) => {
-                        children[5].insert(x - size, y, z - size, node, size, depth - 1)
-                    } // 101
-                    (true, false, false) => {
-                        children[6].insert(x, y - size, z - size, node, size, depth - 1)
-                    } // 011
-                    (false, false, false) => {
-                        children[7].insert(x - size, y - size, z - size, node, size, depth - 1)
-                    } // 111
-                };
+                children[index].insert(x % s, y % s, z % s, node, s, depth - 1)
             }
             Node::Leaf(voxel) => {
                 let voxel = voxel.clone();
                 *self = Node::Branch {
-                    children: Box::new([
-                        Node::Leaf(voxel),
-                        Node::Leaf(voxel),
-                        Node::Leaf(voxel),
-                        Node::Leaf(voxel),
-                        Node::Leaf(voxel),
-                        Node::Leaf(voxel),
-                        Node::Leaf(voxel),
-                        Node::Leaf(voxel),
-                    ]),
+                    children: Box::new(std::array::from_fn(|_| Node::Leaf(voxel))),
                 };
 
                 self.insert(x, y, z, node, size, depth);
